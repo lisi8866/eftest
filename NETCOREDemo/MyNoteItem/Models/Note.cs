ef649cd23f83785ee7ad7f604bff6e14981c956c
shiguang li <10614682@qq.com>
2021-03-31 17:17:48 +0800

    0o

--- NETCOREDemo/MyNoteItem/Models/Note.cs
+++ NETCOREDemo/MyNoteItem/Models/Note.cs
@@ -14,6 +14,9 @@ namespace MyNoteItem.Models
         public int Id { get; set; }
         [Required]
         [MaxLength(100)]
+
+
+
         public string Title { get; set; }
         [Required]
         public string Content { get; set; }
